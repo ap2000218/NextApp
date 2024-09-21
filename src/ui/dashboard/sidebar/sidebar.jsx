dd79b676887c27450d82153d78d51f19a87fc09a
ap2000218 <mf5114796@gmail.com>
2024-09-21 13:58:51 +0100

    Dashboard updated

--- src/ui/dashboard/sidebar/sidebar.jsx
+++ src/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+import styles from "./sidebar.module.css";
+
+
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from "react-icons/md";
+import MenuLink from "./menuLink/menuLink";
+
 const menuItems = [
   {
     title: "Pages",
@@ -63,8 +80,46 @@ const menuItems = [
   },
 ];
 
-const SideBar = () => {
-  return <div>SideBar</div>;
+const Sidebar =  () => {
+  // const { user } = await auth();
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        {/* <Image
+          className={styles.userImage}
+          src={user.img || "/noavatar.png"}
+          alt=""
+          width="50"
+          height="50"
+        /> */}
+        <div className={styles.userDetail}>
+          <span className={styles.username}>Rasheed</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+      {/* <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+      </form> */}
+    </div>
+  );
 };
 
-export default SideBar;
+export default Sidebar;
